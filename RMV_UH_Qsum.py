0eb7dc4cfd97ec2a22dfb4795f1a21abc6df188a
Fantastic Chickens and Where to Find Them (Jenny Mital) <blackberry.jam2@yahoo.com>
2018-01-10 07:56:51 -0800

    Update RMV_UH_Qsum.py
    Changed file to create one worksheet for folder directory. Worksheet has summary page and one tab per subfolder. Old version created one worksheet per subfolder and no summary sheet.

--- RMV_UH_Qsum.py
+++ RMV_UH_Qsum.py
@@ -4,12 +4,15 @@ Created on Sat Jan 06 13:38:42 2018
 
 @author: Jenny
 
-Program to get Qmax from each AES UH file in SINGLE folder. Qmax sorted and summed to get
-totals tributary to San Juan Creek (SJC) and Gobernadora (GOB).
+NEW UPDATED VERSION - ONE SUMMARY WKBK WITH TABS FOR EACH YEAR
 
-Notes: trying to read a .res UH file into excel. Tried openpyxl and csv reader.
-Neither really work; delimiter is not standard, and after data goes through csv reader, it's a list,
-which can't be split or modified because the spaces are also entries in the list.
+Program to get Qmax from each AES UH file in specified subfolders within main folder (base). Qmax sorted and summed to get
+totals tributary to San Juan Creek (SJC) and Gobernadora (GOB). Creates one summary sheet per base folder.
+
+Things that didn't work: Built-in delimiter in csv reader doesn't work because spacing inconsistent and multi-character.
+Data from csv reader is a list. List cannot be modified with split on space because space is part of list.
+Regular read() reads data one character at a time.
+When importing data in pandas, need to set types etc to avoid NaN error. Too much work.
 
 """
 import os
@@ -17,10 +20,13 @@ import openpyxl
 from openpyxl import Workbook
 import csv
 
-base = r"C:\Users\Pizzagirl\Documents\programming\UH_extractQ"
+base = r"H:\pdata\134519\Calcs\Strmwater\PA-3 & PA-4\Hydrology\Local\Existing\Unit Hydrograph"
 
-#pathlist=['2-YR', '5-YR', '10-YR', '25-YR', '50-YR', '100-YR']
-pathlist=['50','100']
+#change to folder names if different
+pathlist=['2-YR', '5-YR', '10-YR', '25-YR', '50-YR', '100-YR']
+
+#for single folder:
+#path=r"H:\pdata\134519\Calcs\Strmwater\PA-3 & PA-4\Hydrology\Local\Existing\Unit Hydrograph\2-YR"
 
 #make new workbook to save data
 wb = Workbook()
@@ -43,17 +49,16 @@ for folder in pathlist:
     os.chdir(path)
     #print "path", path
 
-    #add and active new sheet
     ws=wb.create_sheet(folder)
-    
+
     #set column titles
     ws.cell(row=1, column=1).value="Filename"
     ws.cell(row=1, column=2).value="Max Q, cfs"
     ws.cell(row=1, column=3).value="Watershed"
     ws.cell(row=1, column=5).value="SJC sum"
     ws.cell(row=1, column=6).value="GOB sum"
-    
-    #excel worksheet row iterator
+
+    #excel workbook row iterator
     i=3
 
     #make file name list
@@ -68,7 +73,7 @@ for folder in pathlist:
     GOB_files = []
 
     #print "Gob files should be empty", GOB_files
-    
+
     allfiles = os.listdir(path)
 
     #get *.RES files
@@ -79,7 +84,7 @@ for folder in pathlist:
     for resfile in filelist:
 
         with open(resfile, 'rb') as data:
-    
+
             #reset time series datasets
             vol=[]
             hrs=[]
@@ -87,16 +92,16 @@ for folder in pathlist:
             #delimiter purposely set to tab, which is not in file, in order to put each line in a cell.
             #need csv reader --> just using text
             reader=csv.reader(data, delimiter='\t')
-    
+
             for row in reader:
                 str1 = str(row)
-    
+
                 if "Q" in str1:
                     #strip out other characters
                     hrs.append(str1[0:11].strip("[").strip("'").strip("-"))
                     vol.append(str1[12:25].strip("[").strip("'").strip("-"))
                     Q.append(str1[23:32].strip("[").strip("'").strip("-"))
-    
+
         Qmax=float(max(Q[4:]))
         ws.cell(row=i, column=1).value=resfile
         ws.cell(row=i, column=2).value=Qmax
@@ -106,28 +111,27 @@ for folder in pathlist:
             SJC_sum.append(float(Qmax))
             SJC_files.append(resfile)
             ws.cell(row=i, column=3).value="SJC"
-    
+
         elif resfile[:3] in GOB:
             GOB_sum.append(float(Qmax))
             GOB_files.append(resfile)
             ws.cell(row=i, column=3).value="GOB"
-        
-        Ssummary=sum(SJC_sum)
-        Gsummary=sum(GOB_sum)
-        ws.cell(row=2, column=5).value=Ssummary
-        ws.cell(row=2, column=6).value=Gsummary
-        
+        #update excel worksheet row iterator
         i=i+1
 
-        
+    Ssummary=sum(SJC_sum)
+    Gsummary=sum(GOB_sum)
+    ws.cell(row=2, column=5).value=Ssummary
+    ws.cell(row=2, column=6).value=Gsummary
+
     summary.cell(row=j, column=1).value=folder
     summary.cell(row=j, column=2).value=Ssummary
     summary.cell(row=j, column=3).value=Gsummary
-       
+
+    #update summary sheet iterator
     j=j+1
-    
-wb.save('C:\Users\Pizzagirl\Documents\programming\UH_extractQ\Qsummary.xlsx')
 
+    wkshtname='Qsummary.xlsx'
+    wb.save(os.path.join(base, wkshtname))
 
-
-#print "done"
+print "done"
