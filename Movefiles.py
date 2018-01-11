b61f9e75a6fac4f30ab41ae8fbc34a0db1eae571
Fantastic Chickens and Where to Find Them (Jenny Mital) <blackberry.jam2@yahoo.com>
2018-01-11 06:19:26 -0800

    Update Movefiles.py
    Previously program stopped running when it encountered a same name file in the destination folder. Now it can either replace file or not move file depending on "status" switch.

--- Movefiles.py
+++ Movefiles.py
@@ -7,12 +7,18 @@ Created on Wed Jan 10 21:21:24 2018
 Move files into folders by matching characters in the filename and folder name
 
 NOTE: this currently won't work for 100yr files since the 1 is chopped off.
-Currently stops if any same-name files in destination folder. Fix!
+
+IMPORTANT: toggle status to determine whether files of same name in destination will be replaced.
 
 """
 import re
 import os
 from shutil import move
+
+#status = replace: overwrite destination file of same name
+status = "replace"
+#status = skip: don't move source file if destination file has same name
+#status = "skip"
 
 #location of files that need to be relocated
 srcfolder=r'C:\Users\Pizzagirl\Documents\programming\MOVEFOLDER'
@@ -46,27 +52,38 @@ for file1 in allfile:
         print file1
         
         #extract numbers from filename
-        filenum = int(re.findall(r'\d\d', file1)[0])
-        #print type(filenum)
-      
-        print "filenum", filenum
+        filenum = int(re.findall(r'\d\d', file1)[0]) 
+        #print "filenum", filenum
         
         #this line is supposed to return fldrnum that matches. not sure I trust it.
         matchnum = next(x for x in fldrnum if x == filenum)
-        print "matchnum", type(matchnum)
+        #print "matchnum", type(matchnum)
 
-        #shoud return folder with same numbers as RM filename
+        #should return folder with same numbers as RM filename
         #could just add in "-YR" to each value to make folder name instead of using dictionary
         matchfldr = dictionary[matchnum]
-
-        print "matchfldr", matchfldr
+        #print "matchfldr", matchfldr
         
         dest = os.path.join(destfolder, matchfldr)
-        print "dest", dest
+        #print "dest", dest
         
         origin = os.path.join(srcfolder, file1)
-        print "origin", origin
+        #print "origin", origin
         
-        move(origin, dest)
-        print "moved ", file1
+        #path of hypothetical destination file of same name
+        destfile = os.path.join(dest, file1)
+        #print "destfile", destfile
+        
+        #if destination has file of same name        
+        if os.path.exists(destfile):
+            print "exists"
+            if status == "replace":
+                os.remove(destfile)
+                move(origin, dest)
+                print "moved ", file1
+            #if status == "skip", don't move file
+        else:
+            print "doesn't exist"
+            move(origin, dest)
+            print "moved ", file1
 
