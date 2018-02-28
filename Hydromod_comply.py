6d968fa2fa49c853f7a6beb725fb15c78d4af282
Fantastic Chickens and Where to Find Them (Jenny Mital) <blackberry.jam2@yahoo.com>
2018-02-27 16:22:23 -0800

    Update Hydromod_comply.py
    adding final, fully working version.

--- Hydromod_comply.py
+++ Hydromod_comply.py
@@ -11,12 +11,11 @@
 # ASSUMPTIONS: Q data is continuous, hourly data with no gaps.
 
 import os
-import csv
 import openpyxl
 from openpyxl import Workbook
 from Flow_freq import Qprocess   #does flow frequency analysis
 
-path = r'H:\pdata\160817\Calcs\Strmwater\Water Quality\EPA SWMM 5\Original Tory Walker models'
+path = r'H:\pdata\160817\Calcs\Strmwater\Water Quality\EPA SWMM 5\Add DMA1-2_totalarea=9.43ac'
 
 #Existing condition Q out file from EPA SWMM 5
 EXdata = 'PRE_DEV_POC-1_QOUT.TXT'
@@ -24,6 +23,7 @@ EXdata = 'PRE_DEV_POC-1_QOUT.TXT'
 #Proposed condition Q out file from EPA SWMM 5
 PRdata = 'POST_DEV_POC-1_QOUT.TXT'
 
+os.chdir(path)
 
 #generate existing flow values in 0.1*Q2 - Q10 to compare to:
 EXoutputs = Qprocess(path,EXdata)
@@ -55,19 +55,41 @@ for Qbin in EXQcompare:
     PRcomply.append(PRcount)
     EXcomply.append(EXcount)
 
-    Qfraction = PRcount/EXcount
+    Qfraction = 1.0*PRcount/(1.0*EXcount)
     Qfractionlist.append(Qfraction)
 
     if Qfraction > 1.1:
         print "FAIL: PR/EX:", Qfraction
 
 
-print(Qfractionlist)
+#print("pr", PRcomply)
+#print("/n" "ex", EXcomply)
 
 
-#later: sort to make faster, do plot of results
+#write peak lists to file for comparison using openpyxl --> done
 
+wb = Workbook()
+ws = wb.active
 
+#set column titles
+ws.cell(row=1, column=1).value="Qbin - existing"
+ws.cell(row=1, column=2).value="Existing hours exceeding"
+ws.cell(row=1, column=3).value="Proposed hours exceeding"
+ws.cell(row=1, column=4).value="PR/EX"
+ws.cell(row=1, column=5).value="PASS/FAIL"
 
+i=0
+for Qbin in EXQcompare:
+    ws.cell(row=i+2, column=1).value = EXQcompare[i]
+    ws.cell(row=i+2, column=2).value = EXcomply[i]
+    ws.cell(row=i+2, column=3).value = PRcomply[i]
+    ws.cell(row=i+2, column=4).value = Qfractionlist[i]
+    if Qfraction > 1.1:
+        ws.cell(row=i+2, column=5).value = "FAIL"
+    else:
+        ws.cell(row=i+2, column=5).value = "PASS"
 
+    i+=1
+
+wb.save('Hydromod_compliance'+EXdata+'.xlsx')
 
